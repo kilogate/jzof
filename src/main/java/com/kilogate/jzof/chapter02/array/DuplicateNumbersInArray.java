57457a62df93efbceb6418a1ea48692c8f053a1b
fengquanwei <fengquanwei@corp.netease.com>
2020-02-12 23:42:29 +0800

    数组中重复的数字

---
+++ src/main/java/com/kilogate/jzof/chapter02/array/DuplicateNumbersInArray.java
@@ -0,0 +1,59 @@
+package com.kilogate.jzof.chapter02.array;
+
+import java.util.Arrays;
+
+/**
+ * 数组中重复的数字
+ * <p>
+ * 长度为 n 的数组里的所有数字都在 0 ~ n-1 的范围内，判断数组中是否有重复的数字
+ *
+ * @author fengquanwei
+ * @create 2020/2/12 下午11:10
+ **/
+public class DuplicateNumbersInArray {
+    public static boolean hasDuplicateNumbers(int[] numbers) {
+        if (numbers == null) {
+            return false;
+        }
+
+        int length = numbers.length;
+
+        if (length == 0) {
+            return false;
+        }
+
+        for (int i = 0; i < length; i++) {
+            if (numbers[i] >= length || numbers[i] < 0) {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < length; i++) {
+            while (numbers[i] != i) {
+                if (numbers[i] == numbers[numbers[i]]) {
+                    return true;
+                }
+
+                int temp = numbers[i];
+                numbers[i] = numbers[temp];
+                numbers[temp] = temp;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers1 = null;
+        int[] numbers2 = {};
+        int[] numbers3 = {1, 3, 5};
+        int[] numbers4 = {1, 3, 5, 0, 2, 4};
+        int[] numbers5 = {1, 3, 5, 0, 2, 3};
+
+        System.out.println(String.format("%s hasDuplicateNumbers: %s", Arrays.toString(numbers1), hasDuplicateNumbers(numbers1)));
+        System.out.println(String.format("%s hasDuplicateNumbers: %s", Arrays.toString(numbers2), hasDuplicateNumbers(numbers2)));
+        System.out.println(String.format("%s hasDuplicateNumbers: %s", Arrays.toString(numbers3), hasDuplicateNumbers(numbers3)));
+        System.out.println(String.format("%s hasDuplicateNumbers: %s", Arrays.toString(numbers4), hasDuplicateNumbers(numbers4)));
+        System.out.println(String.format("%s hasDuplicateNumbers: %s", Arrays.toString(numbers5), hasDuplicateNumbers(numbers5)));
+    }
+}
