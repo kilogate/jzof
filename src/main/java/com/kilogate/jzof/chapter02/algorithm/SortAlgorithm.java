3061bafaf881c48250d45b034e448b4ced95f942
kilogate <kilogate@163.com>
2020-03-05 10:36:43 +0800

    update

--- src/main/java/com/kilogate/jzof/chapter02/algorithm/SortAlgorithm.java
+++ src/main/java/com/kilogate/jzof/chapter02/algorithm/SortAlgorithm.java
@@ -12,8 +12,8 @@ public class SortAlgorithm {
     /**
      * 一、冒泡排序
      */
-    public static void bubbleSort(int[] a) {
-        if (a == null || a.length <= 1) {
+    public static void bubbleSort(int[] array) {
+        if (array == null || array.length <= 1) {
             return;
         }
 
@@ -21,16 +21,16 @@ public class SortAlgorithm {
         boolean swap = true;
 
         // 每一趟冒出一个最大元素到尾部，需要 n-1 趟
-        for (int i = 0; swap && i < a.length - 1; i++) {
+        for (int i = 0; swap && i < array.length - 1; i++) {
             swap = false;
 
             // 从头冒到未排序的尾部的前一个元素
-            for (int j = 0; j < a.length - i - 1; j++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
                 // 当前元素要小于后一个元素，否则交换位置
-                if (a[j] > a[j + 1]) {
-                    int temp = a[j];
-                    a[j] = a[j + 1];
-                    a[j + 1] = temp;
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
 
                     swap = true;
                 }
@@ -41,28 +41,28 @@ public class SortAlgorithm {
     /**
      * 二、选择排序
      */
-    public static void selectionSort(int[] a) {
-        if (a == null || a.length <= 1) {
+    public static void selectionSort(int[] array) {
+        if (array == null || array.length <= 1) {
             return;
         }
 
         // 每一趟选出一个最小元素到头部，需要 n-1 趟
-        for (int i = 0; i < a.length - 1; i++) {
+        for (int i = 0; i < array.length - 1; i++) {
             // 最小元素的下标
             int minIndex = i;
 
             // 从未排序的头部元素到尾部元素中选出最小元素
-            for (int j = i + 1; j < a.length; j++) {
-                if (a[j] < a[minIndex]) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
                     minIndex = j;
                 }
             }
 
             // 与未排序的头部元素交互
             if (minIndex != i) {
-                int temp = a[i];
-                a[i] = a[minIndex];
-                a[minIndex] = temp;
+                int temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
             }
         }
     }
@@ -70,47 +70,47 @@ public class SortAlgorithm {
     /**
      * 三、插入排序
      */
-    public static void insertionSort(int[] a) {
-        if (a == null || a.length <= 1) {
+    public static void insertionSort(int[] array) {
+        if (array == null || array.length <= 1) {
             return;
         }
 
         // 依次插入下一个未排序元素
-        for (int i = 0; i < a.length - 1; i++) {
+        for (int i = 0; i < array.length - 1; i++) {
             // 当前未排序元素
-            int current = a[i + 1];
+            int current = array[i + 1];
 
             int preIndex = i;
-            while (preIndex >= 0 && a[preIndex] > current) {
-                a[preIndex + 1] = a[preIndex];
+            while (preIndex >= 0 && array[preIndex] > current) {
+                array[preIndex + 1] = array[preIndex];
                 preIndex--;
             }
 
-            a[preIndex + 1] = current;
+            array[preIndex + 1] = current;
         }
     }
 
     /**
      * 四、希尔排序（一种插入排序，又称缩小增量排序）
      */
-    public static void shellSort(int[] a) {
-        if (a == null || a.length <= 1) {
+    public static void shellSort(int[] array) {
+        if (array == null || array.length <= 1) {
             return;
         }
 
-        int gap = a.length / 2;
+        int gap = array.length / 2;
 
         while (gap > 0) {
-            for (int i = gap; i < a.length; i++) {
-                int current = a[i];
+            for (int i = gap; i < array.length; i++) {
+                int current = array[i];
                 int preIndex = i - gap;
 
-                while (preIndex >= 0 && a[preIndex] > current) {
-                    a[preIndex + gap] = a[preIndex];
+                while (preIndex >= 0 && array[preIndex] > current) {
+                    array[preIndex + gap] = array[preIndex];
                     preIndex -= gap;
                 }
 
-                a[preIndex + gap] = current;
+                array[preIndex + gap] = current;
             }
 
             gap /= 2;
@@ -120,179 +120,182 @@ public class SortAlgorithm {
     /**
      * 五、归并排序
      */
-    public static int[] mergeSort(int[] a) {
-        if (a == null || a.length <= 1) {
-            return a;
+    public static int[] mergeSort(int[] array) {
+        if (array == null || array.length <= 1) {
+            return array;
         }
 
-        int mid = a.length / 2;
+        int mid = array.length / 2;
 
-        int[] left = Arrays.copyOfRange(a, 0, mid);
-        int[] right = Arrays.copyOfRange(a, mid, a.length);
+        int[] leftArray = Arrays.copyOfRange(array, 0, mid);
+        int[] rightArray = Arrays.copyOfRange(array, mid, array.length);
 
-        return doMergeSort(mergeSort(left), mergeSort(right));
+        return doMergeSort(mergeSort(leftArray), mergeSort(rightArray));
     }
 
-    private static int[] doMergeSort(int[] left, int[] right) {
-        int[] result = new int[left.length + right.length];
+    private static int[] doMergeSort(int[] leftArray, int[] rightArray) {
+        int[] resultArray = new int[leftArray.length + rightArray.length];
 
-        int leftIndex = 0;
-        int rightIndex = 0;
-        int resultIndex = 0;
+        // leftArray index
+        int l = 0;
+        // rightArray index
+        int r = 0;
+        // resultArray index
+        int s = 0;
 
-        while (leftIndex < left.length && rightIndex < right.length) {
-            if (left[leftIndex] <= right[resultIndex]) {
-                result[resultIndex] = left[leftIndex];
-                resultIndex++;
-                leftIndex++;
+        while (l < leftArray.length && r < rightArray.length) {
+            if (leftArray[l] <= rightArray[s]) {
+                resultArray[s] = leftArray[l];
+                s++;
+                l++;
             } else {
-                result[resultIndex] = right[rightIndex];
-                resultIndex++;
-                rightIndex++;
+                resultArray[s] = rightArray[r];
+                s++;
+                r++;
             }
         }
 
-        while (leftIndex < left.length) {
-            result[resultIndex] = left[leftIndex];
-            resultIndex++;
-            leftIndex++;
+        while (l < leftArray.length) {
+            resultArray[s] = leftArray[l];
+            s++;
+            l++;
         }
 
-        while (rightIndex < right.length) {
-            result[resultIndex] = right[rightIndex];
-            resultIndex++;
-            rightIndex++;
+        while (r < rightArray.length) {
+            resultArray[s] = rightArray[r];
+            s++;
+            r++;
         }
 
-        return result;
+        return resultArray;
     }
 
     /**
      * 六、快速排序
      */
-    public static void quickSort(int[] a) {
-        if (a == null || a.length <= 1) {
+    public static void quickSort(int[] array) {
+        if (array == null || array.length <= 1) {
             return;
         }
 
-        doQuickSort(a, 0, a.length - 1);
+        doQuickSort(array, 0, array.length - 1);
     }
 
-    private static void doQuickSort(int[] a, int left, int right) {
-        if (a == null || a.length <= 1 || left >= right) {
+    private static void doQuickSort(int[] array, int minIndex, int maxIndex) {
+        if (array == null || array.length <= 1 || minIndex >= maxIndex) {
             return;
         }
 
         // 基准元素
-        int p = a[left];
+        int pivot = array[minIndex];
 
-        int l = left;
-        int r = right;
+        int l = minIndex;
+        int r = maxIndex;
 
         while (l < r) {
             // 从右向左找小于基准元素的元素下标
-            while (l < r && a[r] >= p) {
+            while (l < r && array[r] >= pivot) {
                 r--;
             }
 
-            a[l] = a[r];
+            array[l] = array[r];
 
             // 从左向右找大于基准元素的元素下标
-            while (l < r && a[l] <= p) {
+            while (l < r && array[l] <= pivot) {
                 l++;
             }
 
-            a[r] = a[l];
+            array[r] = array[l];
         }
 
         // 基准元素归位
-        a[l] = p;
+        array[l] = pivot;
 
         // 递归排序基准元素左侧序列
-        doQuickSort(a, left, l - 1);
+        doQuickSort(array, minIndex, l - 1);
 
         // 递归排序基准元素右侧序列
-        doQuickSort(a, r + 1, right);
+        doQuickSort(array, r + 1, maxIndex);
     }
 
     /**
      * 七、堆排序
      */
-    public static void heapSort(int[] a) {
-        if (a == null || a.length <= 1) {
+    public static void heapSort(int[] array) {
+        if (array == null || array.length <= 1) {
             return;
         }
 
-        int length = a.length;
+        int length = array.length;
 
         // 构造最大堆
-        buildMaxHeap(a);
+        buildMaxHeap(array);
 
         while (length > 1) {
             // 交换收尾元素
-            int temp = a[0];
-            a[0] = a[length - 1];
-            a[length - 1] = temp;
+            int temp = array[0];
+            array[0] = array[length - 1];
+            array[length - 1] = temp;
 
             // 未排序数组长度减一
             length--;
 
             // 调整首元素使堆满足最大堆
-            adjustMaxHeap(a, 0, length);
+            adjustMaxHeap(array, 0, length);
         }
     }
 
-    private static void buildMaxHeap(int[] a) {
-        if (a == null || a.length <= 1) {
+    private static void buildMaxHeap(int[] array) {
+        if (array == null || array.length <= 1) {
             return;
         }
 
         // 自底向上自右向左构造最大堆
-        for (int i = a.length - 1; i >= 0; i--) {
-            adjustMaxHeap(a, i, a.length);
+        for (int i = array.length - 1; i >= 0; i--) {
+            adjustMaxHeap(array, i, array.length);
         }
     }
 
     /**
      * 调整元素使堆满足最大堆
      */
-    private static void adjustMaxHeap(int[] a, int i, int length) {
-        if (a == null || a.length <= 1) {
+    private static void adjustMaxHeap(int[] array, int index, int length) {
+        if (array == null || array.length <= 1) {
             return;
         }
 
-        if (length < 1 || length > a.length) {
+        if (length < 1 || length > array.length) {
             return;
         }
 
-        if (i < 0 || i >= length) {
+        if (index < 0 || index >= length) {
             return;
         }
 
 
-        int max = i;
-        int left = 2 * i + 1;
-        int right = 2 * i + 2;
+        int max = index;
+        int left = 2 * index + 1;
+        int right = 2 * index + 2;
 
         // 左子树元素更大？
-        if (left < length && a[left] > a[max]) {
+        if (left < length && array[left] > array[max]) {
             max = left;
         }
 
         // 右子树元素更大？
-        if (right < length && a[right] > a[max]) {
+        if (right < length && array[right] > array[max]) {
             max = right;
         }
 
         // 当前元素不是最大元素？
-        if (max != i) {
+        if (max != index) {
             // 与最大元素交换位置
-            int temp = a[max];
-            a[max] = a[i];
-            a[i] = temp;
+            int temp = array[max];
+            array[max] = array[index];
+            array[index] = temp;
 
             // 继续调整次大元素
-            adjustMaxHeap(a, max, length);
+            adjustMaxHeap(array, max, length);
         }
     }
 
