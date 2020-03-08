22802eea80da0f2d18c4785a74641bee125e8749
kilogate <kilogate@163.com>
2020-03-08 23:24:06 +0800

    剪绳子

---
+++ src/main/java/com/kilogate/jzof/chapter02/algorithm/CutRope.java
@@ -0,0 +1,50 @@
+package com.kilogate.jzof.chapter02.algorithm;
+
+/**
+ * 剪绳子
+ * <p>
+ * 给定长度为n的绳子，把绳子剪成m段，每段长度都是整数，使得所有段长度的乘积最大。
+ * 求最大的乘积。
+ *
+ * @author fengquanwei
+ * @create 2020/3/8 下午10:17
+ **/
+public class CutRope {
+    /**
+     * 求剪绳子方案的最大乘积（动态规划）
+     * f(0) = 1
+     * f(1) = 1
+     * f(n) = max(i*f(n-i)) (0<i<=n)
+     */
+    public static int calculateMaxProduct(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+
+        if (n == 1) {
+            return 1;
+        }
+
+        int[] products = new int[n + 1];
+        products[0] = 1;
+        products[1] = 1;
+
+        // 自底向上求解
+        for (int i = 2; i <= n; i++) {
+            int max = 0;
+            for (int j = 1; j <= i; j++) {
+                int product = j * products[i - j];
+                if (product > max) {
+                    max = product;
+                }
+            }
+            products[i] = max;
+        }
+
+        return products[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculateMaxProduct(8));
+    }
+}
